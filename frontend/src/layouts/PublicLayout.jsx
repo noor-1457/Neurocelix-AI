02c590ff4b320c64ec84f9f8927688d065853480
Noori <noorzafar882@gmail.com>
2026-04-06 22:59:27 +0500

    Footer added

--- frontend/src/layouts/PublicLayout.jsx
+++ frontend/src/layouts/PublicLayout.jsx
@@ -1,15 +1,19 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 export default function PublicLayout({ dark, setDark }) {
   return (
-    <div className="min-h-screen bg-white dark:bg-gray-900 text-black dark:text-white transition-colors duration-500">
-      <Navbar dark={dark} setDark={setDark} />
+    <>
+      <div className=" bg-white dark:bg-gray-900 text-black dark:text-white transition-colors duration-500">
+        <Navbar dark={dark} setDark={setDark} />
 
-      <main>
-        {/* Provide dark context to child pages */}
-        <Outlet context={{ dark, setDark }} />
-      </main>
-    </div>
+        <main>
+          {/* Provide dark context to child pages */}
+          <Outlet context={{ dark, setDark }} />
+        </main>
+      </div>
+      <Footer dark={dark} />
+    </>
   );
 }
